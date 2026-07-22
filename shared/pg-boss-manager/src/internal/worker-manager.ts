ebef7c35776071d357783b7de9dcd622a0b41c0a
Oleksii Temchenko <OTemchenko@fxcess.com>
2026-07-22 20:30:32 +0300

    in the middle of debugging

--- shared/pg-boss-manager/src/internal/worker-manager.ts
+++ shared/pg-boss-manager/src/internal/worker-manager.ts
@@ -14,6 +14,8 @@ import {
     InternalWorkerRegistration,
 } from './types';
 
+import logger from "@shared/logger";
+
 export class WorkerManager {
 
     private readonly workers: InternalWorkerRegistration[] = [];
